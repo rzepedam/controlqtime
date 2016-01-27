41b2a572cbf0dbd04bca387e1ed6c3a1a83ae5bd
ControlQTime <rzepeda@gmail.com>
2016-01-27 09:34:36 -0300

    Form wizard to use Tabs Twitter Bootstrap Wizard

--- resources/views/human-resources/manpowers/create.blade.php
+++ resources/views/human-resources/manpowers/create.blade.php
@@ -14,26 +14,10 @@
 
 @section('content')
 
-    @include('human-resources.manpowers.partials.step_sidebar')
 
-    <div class="box box-solid box-default">
-        <div class="box-header">
-            <h3 class="box-title">Datos Personales</h3>
-        </div><!-- /.box-header -->
-        <div class="box-body">
-
-            {{ Form::open(['route' => 'human-resources.manpowers.store', 'method' => 'POST', 'role' => 'form', 'id' => 'step1']) }}
-
-                @include('human-resources.manpowers.partials.step1.personal_data')
-
-
-            {{ Form::close() }}
-        </div><!-- /.box-body -->
-    </div>
 
 @stop
 
 @section('scripts')
     {{ Html::script('assets/js/jquery.inputmask.js') }}
-    {{ Html::script('me/js/step-form.js') }}
 @stop
