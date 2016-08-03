04748a7dee666345f28cf6715f68ab8bf5318dff
Raul Meza <raulmeza@controlqtime.cl>
2016-08-03 14:55:02 -0400

    Actualizado imagen de perfil. Obtine URL desde Biometry, sino muestra imagen en blaco

--- app/Core/Api/Http/Controllers/AccessControlController.php
+++ app/Core/Api/Http/Controllers/AccessControlController.php
@@ -5,6 +5,7 @@ namespace Controlqtime\Core\Api\Http\Controllers;
 use Controlqtime\Core\Api\Entities\AccessControl;
 use Controlqtime\Core\Api\Http\Requests\AccessControlRequest;
 use Controlqtime\Core\Api\Transformers\AccessControlTransformer;
+use Controlqtime\Core\Contracts\AccessControlRepoInterface;
 use Controlqtime\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Dingo\Api\Routing\Helpers;
@@ -14,6 +15,20 @@ use Webpatser\Uuid\Uuid;
 class AccessControlController extends Controller
 {
     use Helpers;
+
+	/**
+	 * @var AccessControlRepoInterface
+	 */
+	protected $access_control;
+
+	/**
+	 * AccessControlController constructor.
+	 * @param AccessControlRepoInterface $access_control
+	 */
+	public function __construct(AccessControlRepoInterface $access_control)
+	{
+		$this->access_control = $access_control;
+	}
 
 	/**
 	 * @return \Dingo\Api\Http\Response
@@ -33,7 +48,7 @@ class AccessControlController extends Controller
 		{
 			$data          = $request->all();
 			$data['uuid']  = Uuid::generate(4)->string;
-			$accessControl = AccessControl::create($data);
+			$accessControl = $this->access_control->create($data);
 
 			return $this->response->item($accessControl, new AccessControlTransformer());
 		}catch (ModelNotFoundException $e) {
--- resources/views/human-resources/employees/partials/show/personal_information.blade.php
+++ resources/views/human-resources/employees/partials/show/personal_information.blade.php
@@ -12,27 +12,36 @@
                         <div class="col-md-offset-2 col-md-6">
                             <table class="table table-striped table-bordered">
                                 <tbody>
-                                    <tr class="text-center">
-                                        <td><i class="fa fa-street-view"></i> {{ $employee->full_name }}</td>
-                                    </tr>
-                                    <tr class="text-center">
-                                        <td><i class="fa fa-building-o"></i><a href="{{ route('administration.companies.show', $employee->company->id) }}" style="color: #757575"> {{ $employee->company->firm_name }}</a></td>
-                                    </tr>
-                                    <tr class="text-center">
-                                        <td>
-                                            <i class="fa fa-envelope"></i> {{ Html::mailto($employee->email_employee, null, ['class' => 'text-muted']) }}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-center">
-                                            <i class="fa fa-phone" aria-hidden="true"></i> {{ $employee->phone1 }}
-                                        </td>
-                                    </tr>
+                                <tr class="text-center">
+                                    <td><i class="fa fa-street-view"></i> {{ $employee->full_name }}</td>
+                                </tr>
+                                <tr class="text-center">
+                                    <td><i class="fa fa-building-o"></i><a
+                                                href="{{ route('administration.companies.show', $employee->company->id) }}"
+                                                style="color: #757575"> {{ $employee->company->firm_name }}</a></td>
+                                </tr>
+                                <tr class="text-center">
+                                    <td>
+                                        <i class="fa fa-envelope"></i> {{ Html::mailto($employee->email_employee, null, ['class' => 'text-muted']) }}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-center">
+                                        <i class="fa fa-phone" aria-hidden="true"></i> {{ $employee->phone1 }}
+                                    </td>
+                                </tr>
                                 </tbody>
                             </table>
                         </div>
                         <div class="col-md-2" style="margin-top: 3px;">
-                            <img src="https://s3-sa-east-1.amazonaws.com/biometry/faces/2016/06/29/172959168369.jpg" alt="{{ $employee->full_name }}" width="152" height="152" class="img-bordered img-bordered-primary">
+                            @if (! is_null($employee->url))
+                                <img src="{{ $employee->url }}" alt="{{ $employee->full_name }}" width="152"
+                                     height="152" class="img-bordered img-bordered-primary">
+                            @else
+                                <img src="{{ '/storage/employee/500/CertificationEmployee/1/1470247271TdDgw.JPG' }}"
+                                     alt="{{ $employee->full_name }}" width="152" height="152"
+                                     class="img-bordered img-bordered-primary">
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -41,64 +50,66 @@
                     <div class="col-md-offset-1 col-md-10">
                         <table class="table table-striped table-bordered">
                             <tbody>
+                            <tr>
+                                <td class="col-md-3">Rut</td>
+                                <td class="text-center"> {{ $employee->rut }}</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-3">Edad</td>
+                                <td class="text-center">{{ $employee->birthday->age . " años" }}</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-3">Fecha de Nacimiento</td>
+                                <td class="text-center text-capitalize"><i class="fa fa-calendar"
+                                                                           aria-hidden="true"></i> {{ Date::parse($employee->birthday)->format('l j F Y') }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-3">Nacionalidad</td>
+                                <td class="text-center">{{ $employee->nationality->name }}</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-3">Género</td>
+                                <td class="text-center">{{ $employee->gender->name }}</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-3">Domicilio</td>
+                                <td class="text-center"><i class="fa fa-map-marker"
+                                                           aria-hidden="true"></i> {{ $employee->address . ", " . $employee->commune->name . ". " . $employee->commune->province->name . ". " . $employee->commune->province->region->name }}
+                                </td>
+                            </tr>
+                            @if ($employee->depto)
                                 <tr>
-                                    <td class="col-md-3">Rut</td>
-                                    <td class="text-center"> {{ $employee->rut }}</td>
+                                    <td class="col-md-3">Depto</td>
+                                    <td class="text-center"> {{ $employee->depto }}</td>
                                 </tr>
+                            @endif
+                            @if ($employee->depto)
                                 <tr>
-                                    <td class="col-md-3">Edad</td>
-                                    <td class="text-center">{{ $employee->birthday->age . " años" }}</td>
+                                    <td class="col-md-3">Block</td>
+                                    <td class="text-center"> {{ $employee->block }}</td>
                                 </tr>
+                            @endif
+                            @if ($employee->block)
                                 <tr>
-                                    <td class="col-md-3">Fecha de Nacimiento</td>
-                                    <td class="text-center text-capitalize"><i class="fa fa-calendar" aria-hidden="true"></i> {{ Date::parse($employee->birthday)->format('l j F Y') }}
-                                    </td>
+                                    <td class="col-md-3">Casa</td>
+                                    <td class="text-center"> {{ $employee->block }}</td>
                                 </tr>
+                            @endif
+                            @if ($employee->num_hom)
                                 <tr>
-                                    <td class="col-md-3">Nacionalidad</td>
-                                    <td class="text-center">{{ $employee->nationality->name }}</td>
+                                    <td class="col-md-3">Casa</td>
+                                    <td class="text-center"> {{ $employee->num_hom }}</td>
                                 </tr>
-                                <tr>
-                                    <td class="col-md-3">Género</td>
-                                    <td class="text-center">{{ $employee->gender->name }}</td>
-                                </tr>
-                                <tr>
-                                    <td class="col-md-3">Domicilio</td>
-                                    <td class="text-center"><i class="fa fa-map-marker" aria-hidden="true"></i> {{ $employee->address . ", " . $employee->commune->name . ". " . $employee->commune->province->name . ". " . $employee->commune->province->region->name }}
-                                    </td>
-                                </tr>
-                                @if ($employee->depto)
-                                    <tr>
-                                        <td class="col-md-3">Depto</td>
-                                        <td class="text-center"> {{ $employee->depto }}</td>
-                                    </tr>
-                                @endif
-                                @if ($employee->depto)
-                                    <tr>
-                                        <td class="col-md-3">Block</td>
-                                        <td class="text-center"> {{ $employee->block }}</td>
-                                    </tr>
-                                @endif
-                                @if ($employee->block)
-                                    <tr>
-                                        <td class="col-md-3">Casa</td>
-                                        <td class="text-center"> {{ $employee->block }}</td>
-                                    </tr>
-                                @endif
-                                @if ($employee->num_hom)
-                                    <tr>
-                                        <td class="col-md-3">Casa</td>
-                                        <td class="text-center"> {{ $employee->num_hom }}</td>
-                                    </tr>
-                                @endif
-                                <tr>
-                                    <td class="col-md-3">Ingresado</td>
-                                    <td class="text-center text-capitalize">{{ Date::parse($employee->created_at)->format('l j F Y H:i:s') }}</td>
-                                </tr>
-                                <tr>
-                                    <td class="col-md-3">Actualizado</td>
-                                    <td class="text-center text-capitalize">{{ Date::parse($employee->updated_at)->format('l j F Y H:i:s') }}</td>
-                                </tr>
+                            @endif
+                            <tr>
+                                <td class="col-md-3">Ingresado</td>
+                                <td class="text-center text-capitalize">{{ Date::parse($employee->created_at)->format('l j F Y H:i:s') }}</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-3">Actualizado</td>
+                                <td class="text-center text-capitalize">{{ Date::parse($employee->updated_at)->format('l j F Y H:i:s') }}</td>
+                            </tr>
                             </tbody>
                         </table>
                     </div>
@@ -112,7 +123,8 @@
                         <table class="table table-striped table-bordered">
                             <tbody>
                             <tr class="text-center success">
-                                <td class="col-md-12" colspan="6"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                <td class="col-md-12" colspan="6"><i class="fa fa-pencil-square-o"
+                                                                     aria-hidden="true"></i>
                                     Información de Contacto
                                 </td>
                             </tr>
@@ -154,51 +166,51 @@
                     </div>
                 </div>
             </div>
-            <br />
+            <br/>
             <div class="table-responsive">
                 <div class="row">
                     <div class="col-md-offset-1 col-md-10">
                         <?php $i = 1; ?>
                         <table class="table table-striped table-bordered">
                             <tbody>
-                                <tr class="text-center warning">
-                                    <td class="col-md-12" colspan="3"><i class="md-male-female font-size-18"></i>
-                                        Parentescos Familiares
+                            <tr class="text-center warning">
+                                <td class="col-md-12" colspan="3"><i class="md-male-female font-size-18"></i>
+                                    Parentescos Familiares
+                                </td>
+                            </tr>
+                            @if (count($employee->familyRelationships) > 0)
+                                <tr class="active">
+                                    <th class="col-md-1 text-center"><i class="fa fa-hashtag"></i></th>
+                                    <th class="col-md-3 text-center">Relación</th>
+                                    <th class="col-md-8 text-center">Trabajador</th>
+                                </tr>
+                                @foreach($employee->familyRelationships as $family_relationship)
+                                    <tr>
+                                        <td class="text-center">{{ $i }}</td>
+                                        <td class="text-center">{{ $family_relationship->relationship->name }}</td>
+                                        <td class="text-center">{{ $family_relationship->employeeFamily->full_name }}</td>
+                                    </tr>
+                                    <?php $i ++; ?>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td style="border-bottom: hidden; background-color: #FAFAFA"></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="5" class="text-center font-size-18">
+                                        No existen Parentescos Familiares Asociados
                                     </td>
                                 </tr>
-                                @if (count($employee->familyRelationships) > 0)
-                                    <tr class="active">
-                                        <th class="col-md-1 text-center"><i class="fa fa-hashtag"></i></th>
-                                        <th class="col-md-3 text-center">Relación</th>
-                                        <th class="col-md-8 text-center">Trabajador</th>
-                                    </tr>
-                                    @foreach($employee->familyRelationships as $family_relationship)
-                                        <tr>
-                                            <td class="text-center">{{ $i }}</td>
-                                            <td class="text-center">{{ $family_relationship->relationship->name }}</td>
-                                            <td class="text-center">{{ $family_relationship->employeeFamily->full_name }}</td>
-                                        </tr>
-                                        <?php $i ++; ?>
-                                    @endforeach
-                                @else
-                                    <tr>
-                                        <td style="border-bottom: hidden; background-color: #FAFAFA"></td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="5" class="text-center font-size-18">
-                                            No existen Parentescos Familiares Asociados
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="border-top: hidden; background-color: #FAFAFA"></td>
-                                    </tr>
-                                @endif
+                                <tr>
+                                    <td style="border-top: hidden; background-color: #FAFAFA"></td>
+                                </tr>
+                            @endif
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-            <br />
+            <br/>
         </div>
     </div>
 </div>
