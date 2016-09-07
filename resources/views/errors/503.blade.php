c2a2bf5c1d0a795fb36351f9afdb47d99cae1a7f
Raul Meza <raulmeza@controlqtime.cl>
2016-09-07 14:39:08 -0300

    Added Styles for 503 page. Index url => controlqtime.cl y .com

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,20 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="bootstrap admin template">
-    <meta name="author" content="">
+    <meta name="description" content="CQTime - Where questions find answers">
+    <meta name="author" content="Raúl Elías Meza Mora">
     <title>ControlQTime</title>
-    <link rel="shortcut icon" href="{{ asset('me/img/favicon.ico') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-extend.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/site.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/fonts/material-design/material-design.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/fonts/brand-icons/brand-icons.min.css') }}">
-    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,300italic'>
-    <script src="{{ asset('assets/js/breakpoints.js') }}"></script>
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ elixir('css/index-layout-core.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="{{ elixir('css/index-layout-fonts.css') }}">
+    <!-- Browsers Utilities -->
+    <script src="{{ elixir('js/index-layout-browser-utilities.js') }}"></script>
     <script>
-      Breakpoints();
+        Breakpoints();
     </script>
 </head>
 <body class="page-maintenance layout-full">
@@ -34,10 +31,10 @@
                 <p>© 2016. Todos los derechos reservados.</p>
                 <div class="social">
                     <a href="javascript:void(0)">
-                        <i class="icon bd-twitter" aria-hidden="true"></i>
+                        <i class="icon md-twitter" aria-hidden="true"></i>
                     </a>
                     <a href="javascript:void(0)">
-                        <i class="icon bd-facebook" aria-hidden="true"></i>
+                        <i class="icon md-facebook" aria-hidden="true"></i>
                     </a>
                 </div>
             </footer>
