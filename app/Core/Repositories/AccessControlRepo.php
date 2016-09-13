8f9643c37319f4515b99b09b31ea1886a8eaf380
Raul Meza <raulmeza@controlqtime.cl>
2016-09-13 12:25:50 -0300

    Fixed bug for load AccessControl table changed construct AccessControl by AccessControlApi in AccessControlRepo

--- app/Core/Repositories/AccessControlRepo.php
+++ app/Core/Repositories/AccessControlRepo.php
@@ -2,15 +2,15 @@
 
 namespace Controlqtime\Core\Repositories;
 
-use Controlqtime\Core\Contracts\AccessControlRepoInterface;
-use Controlqtime\Core\Entities\AccessControl;
 use Controlqtime\Core\Repositories\Base\BaseRepo;
+use Controlqtime\Core\Api\Entities\AccessControlApi;
+use Controlqtime\Core\Contracts\AccessControlRepoInterface;
 
 class AccessControlRepo extends BaseRepo implements AccessControlRepoInterface
 {
 	protected $model;
 
-	public function __construct(AccessControl $model)
+	public function __construct(AccessControlApi $model)
 	{
 		$this->model = $model;
 	}
