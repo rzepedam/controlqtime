fb4383fc92bdca4fcf9db7106326667d9fd2c4ee
Raúl Elías <rauleliasmezamora@gmail.com>
2016-10-13 17:50:59 -0300

    Code Commented in Show Company. Now Descommented

--- resources/views/administration/companies/partials/show/legal_representative.blade.php
+++ resources/views/administration/companies/partials/show/legal_representative.blade.php
@@ -35,7 +35,26 @@
                                     {{ $company->legalRepresentative->address->address }}{{ ($company->legalRepresentative->address->detailAddressLegalEmployee->depto) ? ', Depto ' . $company->legalRepresentative->address->detailAddressLegalEmployee->depto : '' }}{{ ($company->legalRepresentative->address->detailAddressLegalEmployee->block) ? ', Block ' . $company->legalRepresentative->address->detailAddressLegalEmployee->block : '' }}{{ ($company->legalRepresentative->address->detailAddressLegalEmployee->num_home) ? ', Nº Casa ' . $company->legalRepresentative->address->detailAddressLegalEmployee->num_home : '' }}{{ ". " . $company->legalRepresentative->address->commune->name . ". " . $company->legalRepresentative->address->commune->province->name . ". " . $company->legalRepresentative->address->commune->province->region->name }}
                                 </td>
                             </tr>
-
+                            <tr>
+                                <td>Teléfono 1</td>
+                                <td class="text-center"><i class="fa fa-phone"></i>
+                                    {{ $company->legalRepresentative->address->phone1 }}
+                                </td>
+                            </tr>
+                            @if($company->legalRepresentative->address->phone2)
+                                <tr>
+                                    <td>Teléfono 2</td>
+                                    <td class="text-center">
+                                        {{ $company->legalRepresentative->address->phone2 }}
+                                    </td>
+                                </tr>
+                            @endif
+                            <tr>
+                                <td>Email</td>
+                                <td class="text-center">
+                                    <i class="fa fa-envelope"></i> {{ Html::mailto($company->legalRepresentative->email_representative, null, ['class' => 'text-muted']) }}
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
