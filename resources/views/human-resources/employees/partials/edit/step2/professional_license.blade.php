f6f0c8cb3b9e97573ebe52d3b8dd38762b50e737
Raúl Elías <rauleliasmezamora@gmail.com>
2016-10-28 12:20:09 -0300

    More tests in ipad

--- resources/views/human-resources/employees/partials/edit/step2/professional_license.blade.php
+++ resources/views/human-resources/employees/partials/edit/step2/professional_license.blade.php
@@ -14,40 +14,32 @@
         </div>
         <div class="row">
             <div class="col-md-1 hide">
-                <div class="form-group">
-                    {{ Form::label("id_professional_license", "ID", ["class" => "control-label"]) }}
-                    {{ Form::text("id_professional_license[]", $professional_license->id, ["id" => "id_professional_license" . $i, "class" => "form-control"]) }}
-                </div>
+                {{ Form::label("id_professional_license", "ID", ["class" => "control-label"]) }}
+                {{ Form::text("id_professional_license[]", $professional_license->id, ["id" => "id_professional_license" . $i, "class" => "form-control"]) }}
             </div>
-            <div class="col-sm-4 col-md-3">
-                <div class="form-group">
-                    {{ Form::label('type_professional_license_id', 'Tipo Licencia') }}
-                    {{ Form::select('type_professional_license_id[]', $type_professional_licenses, $professional_license->type_professional_license_id, ['class'=> 'form-control']) }}
-                </div>
+            <div class="col-sm-6 col-md-3 form-group">
+                {{ Form::label('type_professional_license_id', 'Tipo Licencia') }}
+                {{ Form::select('type_professional_license_id[]', $type_professional_licenses, $professional_license->type_professional_license_id, ['class'=> 'form-control']) }}
             </div>
-            <div class="col-sm-3 col-md-2">
-                <div class="form-group">
-                    {{ Form::label('emission_license', 'Fecha de Emisión') }}
-                    <div class="input-group date beforeCurrentDate">
-                        <div class="input-group-addon">
-                            <i class="fa fa-calendar"></i>
-                        </div>
-                        {{ Form::text('emission_license[]', $professional_license->emission_license->format('d-m-Y'), ['class'=> 'form-control', 'readonly']) }}
+            <div class="col-sm-6 col-md-2 form-group">
+                {{ Form::label('emission_license', 'Fecha Emisión') }}
+                <div class="input-group date beforeCurrentDate">
+                    <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
                     </div>
+                    {{ Form::text('emission_license[]', $professional_license->emission_license->format('d-m-Y'), ['class'=> 'form-control text-center', 'readonly']) }}
                 </div>
             </div>
-            <div class="col-sm-3 col-md-2">
-                <div class="form-group">
-                    {{ Form::label('expired_license', 'Fecha de Vencimiento') }}
-                    <div class="input-group date afterCurrentDate">
-                        <div class="input-group-addon">
-                            <i class="fa fa-calendar"></i>
-                        </div>
-                        {{ Form::text('expired_license[]', $professional_license->expired_license->format('d-m-Y'), ['class'=> 'form-control', 'readonly']) }}
+            <div class="col-sm-6 col-md-2 form-group">
+                {{ Form::label('expired_license', 'Fecha Expiración') }}
+                <div class="input-group date afterCurrentDate">
+                    <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
                     </div>
+                    {{ Form::text('expired_license[]', $professional_license->expired_license->format('d-m-Y'), ['class'=> 'form-control text-center', 'readonly']) }}
                 </div>
             </div>
-            <div class="col-md-offset-2 col-sm-2 col-md-2 form-group">
+            <div class="col-md-offset-2 col-sm-6 col-md-2 form-group text-center">
                 {{ Form::label("is_donor" . $i, "Es donante?") }}
                 <ul class="list-unstyled list-inline">
                     <li>
@@ -66,11 +58,9 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-12 col-md-12">
-                <div class="form-group">
-                    {{ Form::label('detail_license', 'Detalle') }}
-                    {{ Form::textarea('detail_license[]', $professional_license->detail_license, ['class'=> 'form-control', 'rows'=> 3]) }}
-                </div>
+            <div class="col-sm-12 col-md-12 form-group">
+                {{ Form::label('detail_license', 'Detalle') }}
+                {{ Form::textarea('detail_license[]', $professional_license->detail_license, ['class'=> 'form-control', 'rows'=> 3]) }}
             </div>
         </div>
         <br/>
