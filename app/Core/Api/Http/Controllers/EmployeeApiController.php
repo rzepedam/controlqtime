11f1a73428e610e20041dfb362eb2467ec18a32a
Raúl Elías <rauleliasmezamora@gmail.com>
2016-12-28 12:46:16 -0300

    Fix error when image employee is update by Biometry

--- app/Core/Api/Http/Controllers/EmployeeApiController.php
+++ app/Core/Api/Http/Controllers/EmployeeApiController.php
@@ -3,6 +3,7 @@
 namespace Controlqtime\Core\Api\Http\Controllers;
 
 use Exception;
+use Illuminate\Log\Writer as Log;
 use Controlqtime\Core\Entities\Employee;
 use Controlqtime\Http\Controllers\Controller;
 use Controlqtime\Core\Api\Http\Request\EmployeeApiRequest;
@@ -15,13 +16,20 @@ class EmployeeApiController extends Controller
 	protected $employee;
 	
 	/**
+	 * @var Log
+	 */
+	protected $log;
+	
+	/**
 	 * EmployeeApiController constructor.
 	 *
 	 * @param Employee $employee
+	 * @param Log      $log
 	 */
-	public function __construct(Employee $employee)
+	public function __construct(Employee $employee, Log $log)
 	{
 		$this->employee = $employee;
+		$this->log      = $log;
 	}
 	
 	/**
@@ -34,13 +42,15 @@ class EmployeeApiController extends Controller
 	{
 		try
 		{
-			$employee      = $this->employee->whereFirst('rut', $request->get('rut'));
+			$employee      = $this->employee->where('rut', $request->get('rut'))->firstOrFail();
 			$employee->url = $request->get('url');
 			$employee->save();
 			
-		} catch (Exception $exception)
+		} catch ( Exception $e )
 		{
-			throw new Exception($exception);
+			$this->log->error("Error Update Image Employee: " . $e->getMessage());
+			
+			return response()->json(['status' => false]);
 		}
 		
 		return response()->json([
