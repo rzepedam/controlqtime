adde9b5258f861361f8a99c6c621ff281cedc0ee
Raúl Elías <raulmeza@controlqtime.cl>
2017-08-10 14:09:02 -0400

    xxx

--- app/Notifications/Assistance/WeeklyAssistance.php
+++ app/Notifications/Assistance/WeeklyAssistance.php
@@ -11,12 +11,16 @@ class WeeklyAssistance extends Notification
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
+    protected $user;
+
+	/**
+	 * Create a new notification instance.
+	 *
+	 * @param $user
+	 */
+    public function __construct($user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -41,7 +45,7 @@ class WeeklyAssistance extends Notification
         return (new MailMessage)
                     ->line('The introduction to the notification.')
                     ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->line("Thank you for using our application");
     }
 
     /**
