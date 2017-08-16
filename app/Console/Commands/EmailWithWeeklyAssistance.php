216f3e3eb26b9784b160b39d161220af20023f1a
Raúl Elías <raulmeza@controlqtime.cl>
2017-08-16 15:00:48 -0300

    xxx

--- app/Console/Commands/EmailWithWeeklyAssistance.php
+++ app/Console/Commands/EmailWithWeeklyAssistance.php
@@ -73,6 +73,7 @@ class EmailWithWeeklyAssistance extends Command
 
 		foreach ( $employees as $employee )
 		{
+			dd($employee);
 			Notification::send($employee, new Test());
 			break;
 			$assistances = $assistancesAux
