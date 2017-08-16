2a678a8c4beaf684c4757efd1e83a4e183f5160b
Raúl Elías <raulmeza@controlqtime.cl>
2017-08-16 15:15:13 -0300

    Xxx

--- app/Mail/TestEmail.php
+++ app/Mail/TestEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class TestEmail extends Mailable
+class TestEmail extends Mailable implements ShouldQueue
 {
 	use Queueable, SerializesModels;
 
