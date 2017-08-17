9a834172f1e1ff5aad017297455d77d379e3074a
Raúl Elías <raulmeza@controlqtime.cl>
2017-08-17 15:42:46 -0300

    xxx

--- app/Console/Commands/EmailWithWeeklyAssistance.php
+++ app/Console/Commands/EmailWithWeeklyAssistance.php
@@ -73,7 +73,8 @@ class EmailWithWeeklyAssistance extends Command
 				->where('employee_id', $employee->id)
 				->values()
 				->groupBy(function ($item, $key) {
-					dd($item->created_at->format('d-m'));
+					dd($item);
+					dd(Carbon::createFromFormat('Y-m-d H:i:s', $item->created_at)->format('d-m'));
 					$t = $item->created_at->format('d-m');
 					dd($t);
 					return $t;
