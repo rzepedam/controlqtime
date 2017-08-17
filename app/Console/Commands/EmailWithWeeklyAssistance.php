29e79c63002f7e07ef81228b491b99b1311e829c
Raúl Elías <raulmeza@controlqtime.cl>
2017-08-17 11:11:11 -0300

    xxx

--- app/Console/Commands/EmailWithWeeklyAssistance.php
+++ app/Console/Commands/EmailWithWeeklyAssistance.php
@@ -59,12 +59,15 @@ class EmailWithWeeklyAssistance extends Command
 	 */
 	public function handle()
 	{
-		// $init = Carbon::now()->startOfWeek()->toDateString();
-		// $end  = Carbon::now()->startOfWeek()->addDays(4)->toDateString() . ' 23:59:59';
+		$init = Carbon::now()->startOfWeek()->toDateString();
+		$end  = Carbon::now()->startOfWeek()->addDays(4)->toDateString() . ' 23:59:59';
 
 		$date = \Carbon\Carbon::parse('2017-08-10 00:00:00');
-		$init = $date->toDateString() . ' 00:00:00';
-		$end  = $date->addDays(4)->toDateString() . ' 23:59:59';
+		$init2 = $date->toDateString() . ' 00:00:00';
+		$end2 = $date->addDays(4)->toDateString() . ' 23:59:59';
+
+		dd($init, $end, $init2, $end2);
+
 		$employees = $this->employee->with([
 			'contract.company.address.detailAddressCompany', 'contract.company.address.commune.province.region'
 		])->get();
