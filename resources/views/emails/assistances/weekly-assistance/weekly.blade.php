0edcddd0b33cf2d82960b1d39c4b7b2fffa41af0
Raúl Elías <raulmeza@controlqtime.cl>
2017-08-17 16:11:30 -0300

    xxx

--- resources/views/emails/assistances/weekly-assistance/weekly.blade.php
+++ resources/views/emails/assistances/weekly-assistance/weekly.blade.php
@@ -8,7 +8,37 @@
         <th align="center">Salida</th>
         <th align="center">Hrs. Efectivas</th>
     </tr>
-    @foreach($assistances as $assistance)
+    <tr>
+        <td align="left">Lunes 07 Agosto</td>
+        <td align="center">08:50:55</td>
+        <td align="center">17:55:10</td>
+        <td align="center">9 hrs 10 min</td>
+    </tr>
+    <tr>
+        <td align="left">Martes 08 Agosto</td>
+        <td align="center">08:30:42</td>
+        <td align="center">18:20:10</td>
+        <td align="center">10 hrs 20 min</td>
+    </tr>
+    <tr>
+        <td align="left">Miércoles 9 Agosto</td>
+        <td align="center">09:15:02</td>
+        <td align="center">18:01:46</td>
+        <td align="center">9 hrs 02 min</td>
+    </tr>
+    <tr>
+        <td align="left">Jueves 10 Agosto</td>
+        <td align="center">08:57:19</td>
+        <td align="center">17:58:49</td>
+        <td align="center">8 hrs 50 min</td>
+    </tr>
+    <tr>
+        <td align="left">Viernes 11 Agosto</td>
+        <td align="center">10:25:09</td>
+        <td align="center">18:30:40</td>
+        <td align="center">8 hrs 40 min</td>
+    </tr>
+    {{--@foreach($assistances as $assistance)
         @php
             $effectiveHrs = $assistance->last()->created_at->diff($assistance->first()->created_at)
         @endphp
@@ -20,5 +50,5 @@
             <td align="center">{{ $assistance->last()->created_at->format('H:i:s') }}</td>
             <td align="center">{{ $effectiveHrs->h . " hrs. " . $effectiveHrs->i . "min." }}</td>
         </tr>
-    @endforeach
+    @endforeach--}}
 </table>
