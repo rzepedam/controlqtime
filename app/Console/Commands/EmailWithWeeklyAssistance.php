0654713b1713ac85202fcacea4b5c7fee3745358
Raúl Elías <raulmeza@controlqtime.cl>
2017-08-18 12:16:51 -0300

    xxx

--- app/Console/Commands/EmailWithWeeklyAssistance.php
+++ app/Console/Commands/EmailWithWeeklyAssistance.php
@@ -71,8 +71,9 @@ class EmailWithWeeklyAssistance extends Command
 					return Carbon::createFromFormat('Y-m-d H:i:s', $item->created_at)->format('d-m');
 				});
 
-			$message = (new Weekly($assistances, $employee, $init, $end))->onQueue('emails');
-			Mail::to($employee->email_employee)->queue($message);
+			//$message = (new Weekly($assistances, $employee, $init, $end))->onQueue('emails');
+			//Mail::to($employee->email_employee)->queue($message);
+			Mail::to($employee->email_employee)->send(new TestMail($assistances, $employee, $init, $end));
 			break;
 		}
 	}
